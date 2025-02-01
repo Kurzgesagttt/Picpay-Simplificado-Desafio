82fd259dc6e7151e0d80052659a7535039bdd4f3
Kurzgesagt <lucasmorenorodrigues@hotmail.com>
2025-02-01 19:14:18 -0300

    TODO Controllers

--- src/main/java/com/picPaySimplificado/picpaySimplificado/controller/UserController.java
+++ src/main/java/com/picPaySimplificado/picpaySimplificado/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.picPaySimplificado.picpaySimplificado.controller;
 
+import com.picPaySimplificado.picpaySimplificado.model.user.User;
 import com.picPaySimplificado.picpaySimplificado.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,10 @@ public class UserController {
     private UserService service;
 
     @PostMapping
+    public ResponseEntity<User> createUser(UserDTO user){
+        //TODO
+
+    }
 
 
 }
